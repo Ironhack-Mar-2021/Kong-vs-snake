9f06c4c0c0dc9f8ff9644ae24d12d3464accc575
Joe <joeyballesteros.wfg@gmail.com>
2021-04-09 15:02:32 -0700

    with sound

--- dom.js
+++ dom.js
@@ -1,6 +1,6 @@
 document.querySelector('#vsimg button').onclick = function () {
     document.querySelector('#canvas').classList.toggle('hidden');
-    //     audio.play();
+    audio.play();
 }
 const start = document.querySelector('#start');
 
